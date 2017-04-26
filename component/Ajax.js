17bb97b12df2739ecc9bd96ec13cbf5e2e6b69dc
Dmitri <d@eurekaking.com>
2017-04-26 23:37:03 +0700

    AJAX request mods to access watson (requires disabling browser security for CORS)

--- component/Ajax.js
+++ component/Ajax.js
@@ -1,6 +1,23 @@
-function Ajax(url, fn) { 
-	var request = new XMLHttpRequest();
-	request.open('GET', url, true);
+function Ajax(url, fn) {
+  var request = new XMLHttpRequest();
+  var method = "GET";
+  
+  if ("withCredentials" in request) {
+    // CORS supported
+  } else if (typeof XDomainRequest != "undefined") {
+    // IE
+    request = new XDomainRequest();
+  } else { console.log("CORS is not supported"); }
+  
+  
+  
+	request.open(method, url);
+	request.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+	request.setRequestHeader("Access-Control-Allow-Origin", "*");
+	request.setRequestHeader("Access-Control-Allow-Credentials", "true");
+	request.setRequestHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400) {
 			var data = JSON.parse(request.responseText);
@@ -13,4 +30,5 @@ function Ajax(url, fn) {
 		console.log("Error. Server connection FAIL.", request.responseText);
 	};
 	request.send();
-};
+
+}
